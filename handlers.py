c542fd42dad439885e2edf341b0286bd3dbc797e
cardan4ik <54413131+cardan4ik@users.noreply.github.com>
2020-04-03 18:07:26 +0300

    Add files via upload

---
+++ handlers.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+
+class InvalidCommand(Exception):
+    # создаем новый объект типа эксепшен
+    # Чтобы описать собственный тип исключения, нужно создать новый класс, унаследованный от базового типа Exception.
+    # Это позволит запускать особые виды исключений в ситуациях, когда поведение пользователя не соответствует
+    # алгоритму программы. В конструкторе Exception указываем текст исключения. После того, как оно сработало и было
+    # перехвачено, можно его получить с помощью str.
+    pass  # Оператор-заглушка, равноценный отсутствию операции.
+
+
+def parse_input():
+    raw_input = input('Type command:\n')
+    cmd, *args = raw_input.split(" ")  # получаем данные из введенных, отделяем первое слово, пробел и остаток если есть
+    # Раскрытие в *args поддерживается только в питоне 3, для неименованных
+    # аргументов, * - говорит о том что мы принимаем несколько аргументов в это имя
+    cmd = cmd.upper()  # получаем "CMD"
+    try:  # делаем проверку на получение верной команды
+        if cmd not in AVAILABLE_COMMANDS.keys() and cmd not in EXIT_COMMANDS:  # если нигде нет нашей команды то..
+            raise InvalidCommand("Err Type command: {} this command is not available".format(cmd))
+            #  ..запускаем собственный тип исключения
+    except InvalidCommand as e:  # обрабатываем исключение
+        print(e)
+    return cmd, args  # .. иначе возвращвем полученные данные
+
+
+def namein(args):
+    try:
+        name = args[0].upper()  # делим полученные данные на имя и информацию
+        return name
+    except Exception as err:
+        print('Ошибка ввода данных:', err)
+        return
+
+
+def handle_set(args, storage):  # Обработчик запроса
+    name = namein(args)
+    data = 'none'
+    if name in storage.keys():
+        print('Имя: {}, уже существует!'.format(name))
+        # спросить о перезаписи и воткнуть функцию замены или чего там еще !   !   !  ! ! ! !!!
+    else:
+        if len(args) > 1:
+            data = args[1]
+        if '' != str(name) != 'None':
+            storage[name] = data
+    return storage
+
+
+def handle_get(args, storage):
+    name = namein(args)
+    if name in storage.keys():
+        print('info:', storage[name])
+    else:
+        print('Имя не найдено!')
+    return storage
+
+
+def handle_unset(args, storage):
+    name = namein(args)
+    if name in storage.keys():
+        print('Имя удалено')
+        del storage[name]
+    else:
+        print('Имя не найдено!')
+    return storage
+
+
+def handle_counts(args, storage):
+    quantity = 0
+    total = 0
+    for i in storage:
+        quantity = quantity + 1
+        if storage[i].isdigit():
+            total = total + int(storage[i])
+    print('Записей:', quantity, 'Итого:', total)
+    return storage
+
+
+def handle_find(args, storage):
+    name = namein(args)
+    for i in storage:
+        if storage[i] == name:
+            print(i)
+    return storage
+
+
+AVAILABLE_COMMANDS = {   # словарь (хранилище) всех доступных обработчиков
+    'SET': handle_set,
+    'GET': handle_get,
+    'UNSET': handle_unset,
+    'COUNTS': handle_counts,
+    'FIND': handle_find
+}
+EXIT_COMMANDS = {'', ' ', 'END'}  # Множество значений для выхода
+# Множество в python - "контейнер", содержащий не повторяющиеся элементы в случайном порядке.
---
+++ start.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+from handlers import (parse_input, EXIT_COMMANDS, AVAILABLE_COMMANDS)
+
+
+def run():
+    print("Start running")  # печать "Start running"
+    storage = {}  # Здесь создаем хранилище для значений (словарь)
+    while True:  # Работаем пока есть флаг (запускаем цикл)
+        cmd, args = parse_input()  # Обращаемся к функции, парсим ввод, нас интересует первое слово и остаток
+        if cmd in EXIT_COMMANDS:  # Если команда на выход, то выставляем флаг в false и выходим на следующей итерации
+            break
+        else:
+            try:
+                storage = AVAILABLE_COMMANDS[cmd](args, storage)  # Достаем нужный обработчик из хранилища обработчиков
+            except Exception as err:
+                print('Ошибка при запуске {}:'.format(cmd), err)  # выводим ошибку которая возникает при запуске функций
+    print("Exited.")
+
+
+if __name__ == "__main__":  # проверяем условия что данную программу запускаем напрямую
+    run()  # обращаемся к функции run()
